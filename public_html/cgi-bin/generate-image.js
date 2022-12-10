38a9d5a09394cbc2d6d3757106c4d014a0950cee
wa5ina <ren-nasr@student.1337.ma>
2022-12-10 21:50:03 +0100

    ..

--- public_html/cgi-bin/generate-image.js
+++ public_html/cgi-bin/generate-image.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 const text = args[2];
 
-const API_KEY = "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"
+const API_KEY = "fuck off"
 const axios = require("axios");
 
 axios({
@@ -21,9 +21,10 @@ axios({
     }
 }).then((response) => {
     const url = response['data'];
-    console.error(url);
+    // console.error(url.data[0].url);
     console.log(url.data[0].url);
 }).catch((error) => {
     url = "https://i.kym-cdn.com/entries/icons/original/000/021/464/14608107_1180665285312703_1558693314_n.jpg";
+    // console.error(url);
     console.log(url);
 });
